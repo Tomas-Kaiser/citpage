1d573ae242ec6a18fb6bfe9a0ee22069529f220e
Tomas Kaiser <tomas.kaiser@optum.com>
2024-06-06 20:05:57 +0100

    test form

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Flex, FormLabel, Heading, Text, Textarea } from "@chakra-ui/react"
+import {   Flex,  Heading, Text, } from "@chakra-ui/react"
 
 const ContactPage = () => {
   return (
     <Flex p={{ base: 5, md: 8, lg: 12 }} mt={{ base: 20 }} direction="column" align="center" justify="center">
     <Heading id="contact-form" pb={5} color='#176734'>When do you want me to do a very first lecture?</Heading>
     <Text pb={12} color='#176734'>Give me a date</Text>
-    <Box as="form" w={{ base: '280px', sm: '400px', md: '500px', lg: '750px' }} name="contact-cloey" method="post" data-netlify="true">
+    {/* <Box as="form" w={{ base: '280px', sm: '400px', md: '500px', lg: '750px' }} name="contact-cloey" method="post" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <Box mb="3">
             <FormLabel color='#176734'>Your suggestions</FormLabel>
@@ -14,7 +14,17 @@ const ContactPage = () => {
         <Flex justify="center">
             <Button type="submit" bg='#176734' _hover={{ bg: '#1e9c4b' }} style={{ width: '100%', color: '#f7fafc' }}>Send</Button>
         </Flex>
-    </Box>
+    </Box> */}
+
+
+    <form name="contact-cloey" method="POST" data-netlify="true">
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 </Flex>
   )
 }
